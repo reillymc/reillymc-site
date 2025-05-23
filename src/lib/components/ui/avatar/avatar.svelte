1c436f9fd2bff7fd54a1af3a425c4268b10e6136
reillymc <reilly@mackenzie-cree.net>
2025-05-23 19:58:12 +0000

    Set up base shadcn-svelte components

---
+++ src/lib/components/ui/avatar/avatar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { cn } from "$lib/utils.js";
+	import { Avatar as AvatarPrimitive } from "bits-ui";
+
+	let {
+		ref = $bindable(null),
+		variant,
+		class: className,
+		...restProps
+	}: AvatarPrimitive.RootProps & { variant?: "square" } = $props();
+</script>
+
+<AvatarPrimitive.Root
+	bind:ref
+	data-slot="avatar"
+	class={cn(
+		`relative flex size-8 shrink-0 overflow-hidden rounded-${variant === "square" ? "md" : "full"}`,
+		className,
+	)}
+	{...restProps}
+/>
